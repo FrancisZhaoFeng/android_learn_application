95bb03d3fa87f34da0f62118dee450c03245c2e3
FrancisZhaoFeng <zgfailmr@163.com>
2015-07-20 21:30:55 +0800

    Tab的Fragment切换
    1.屏幕的下方显示一行Tab标签选项，点击不同的标签就可以切换到不同的界面。
    2.Android就已经将ActivityGroup废弃掉了，并且官方推荐的替代方式就是使用Fragment，因为它使用起来更加的灵活。
    备注：http://blog.csdn.net/guolin_blog/article/details/13171191

---
+++ AndroidFrament_Tab/src/com/meizu/androidframent_tab/MainActivity.java
@@ -0,0 +1,167 @@
+package com.meizu.androidframent_tab;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	private MessageFragment messageFragment;
+	private ContactsFragment contactsFragment;
+	private NewsFragment newsFragment;
+	private SettingFragment settingFragment;
+	private View messageLayout;
+	private View contactsLayout;
+	private View newsLayout;
+	private View settingLayout;
+	private ImageView messageImage;
+	private ImageView contactsImage;
+	private ImageView newsImage;
+	private ImageView settingImage;
+	private TextView messageText;
+	private TextView contactsText;
+	private TextView newsText;
+	private TextView settingText;
+	private FragmentManager fragmentManager;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		initView();
+		fragmentManager = getFragmentManager();
+		setTabSelection(0);
+	}
+
+	private void initView() {
+		messageLayout = this.findViewById(R.id.id_tab_message);
+		contactsLayout = this.findViewById(R.id.id_tab_contacts);
+		newsLayout = this.findViewById(R.id.id_tab_news);
+		settingLayout = this.findViewById(R.id.id_tab_setting);
+		messageImage = (ImageView) this.findViewById(R.id.id_message_image);
+		contactsImage = (ImageView) this.findViewById(R.id.id_contacts_image);
+		newsImage = (ImageView) this.findViewById(R.id.id_news_image);
+		settingImage = (ImageView) this.findViewById(R.id.id_setting_image);
+		messageText = (TextView) this.findViewById(R.id.id_message_text);
+		contactsText = (TextView) this.findViewById(R.id.id_contacts_text);
+		newsText = (TextView) this.findViewById(R.id.id_news_text);
+		settingText = (TextView) this.findViewById(R.id.id_setting_text);
+		messageLayout.setOnClickListener(this);
+		contactsLayout.setOnClickListener(this);
+		newsLayout.setOnClickListener(this);
+		settingLayout.setOnClickListener(this);
+	}
+
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.id_tab_message:
+			setTabSelection(0);
+			break;
+		case R.id.id_tab_contacts:
+			setTabSelection(1);
+			break;
+
+		case R.id.id_tab_news:
+			setTabSelection(2);
+			break;
+		case R.id.id_tab_setting:
+			setTabSelection(3);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	private void setTabSelection(int index) {
+		clearSelection();
+		FragmentTransaction transaction = fragmentManager.beginTransaction();
+		hideFragments(transaction);
+		switch (index) {
+		case 0:
+			messageImage.setImageResource(R.drawable.message_selected);
+			messageText.setTextColor(Color.WHITE);
+			if (messageFragment == null) {
+				messageFragment = new MessageFragment();
+				transaction.add(R.id.id_display_content, messageFragment);
+			} else {
+				transaction.show(messageFragment);
+			}
+			break;
+		case 1:
+			contactsImage.setImageResource(R.drawable.contacts_selected);
+			contactsText.setTextColor(Color.WHITE);
+			if (contactsFragment == null) {
+				contactsFragment = new ContactsFragment();
+				transaction.add(R.id.id_display_content, contactsFragment);
+			} else {
+				transaction.show(contactsFragment);
+			}
+			break;
+		case 2:
+			newsImage.setImageResource(R.drawable.news_selected);
+			newsText.setTextColor(Color.WHITE);
+			if (newsFragment == null) {
+				newsFragment = new NewsFragment();
+				transaction.add(R.id.id_display_content, newsFragment);
+			} else {
+				transaction.show(newsFragment);
+			}
+			break;
+		case 3:
+			settingImage.setImageResource(R.drawable.setting_selected);
+			settingText.setTextColor(Color.WHITE);
+			if (settingFragment == null) {
+				settingFragment = new SettingFragment();
+				transaction.add(R.id.id_display_content, settingFragment);
+			} else {
+				transaction.show(settingFragment);
+			}
+			break;
+		}
+		transaction.commit();
+	}
+
+	/**
+	 * 清除掉所有的选中状态。
+	 */
+	private void clearSelection() {
+		messageImage.setImageResource(R.drawable.message_unselected);
+		messageText.setTextColor(Color.parseColor("#82858b"));
+		contactsImage.setImageResource(R.drawable.contacts_unselected);
+		contactsText.setTextColor(Color.parseColor("#82858b"));
+		newsImage.setImageResource(R.drawable.news_unselected);
+		newsText.setTextColor(Color.parseColor("#82858b"));
+		settingImage.setImageResource(R.drawable.setting_unselected);
+		settingText.setTextColor(Color.parseColor("#82858b"));
+	}
+
+	/**
+	 * 将所有的Fragment都置为隐藏状态。
+	 * 
+	 * @param transaction
+	 *            用于对Fragment执行操作的事务
+	 */
+	private void hideFragments(FragmentTransaction transaction) {
+		if (messageFragment != null) {
+			transaction.hide(messageFragment);
+		}
+		if (contactsFragment != null) {
+			transaction.hide(contactsFragment);
+		}
+		if (newsFragment != null) {
+			transaction.hide(newsFragment);
+		}
+		if (settingFragment != null) {
+			transaction.hide(settingFragment);
+		}
+	}
+}
