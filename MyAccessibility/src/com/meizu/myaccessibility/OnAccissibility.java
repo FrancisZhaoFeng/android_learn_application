8c0ad67ce0f8b53f916ad0e9a0e7bb27b28ba9dc
FrancisZhaoFeng <francis_zhaogf@163.com>
2015-09-23 12:43:50 +0800

    添加项目（accessibility）学习

---
+++ MyAccessibility/src/com/meizu/myaccessibility/OnAccissibility.java
@@ -0,0 +1,18 @@
+package com.meizu.myaccessibility;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class OnAccissibility extends AccessibilityService {
+	@Override
+	public void onAccessibilityEvent(AccessibilityEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onInterrupt() {
+		// TODO Auto-generated method stub
+		
+	}
+}
